15e64cbf37c8d7100e74a08d1e172f11256d638e
Tim Hacker <tim.hacker@madetech.com>
2023-09-29 17:59:24 +0100

    First few tests completed as an ensemble Week 1 of the Engineering Book Club
    We were halfway through the 4th test

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -10,7 +10,7 @@ describe('Bottles', () => {
     expect(new Bottles().verse(99)).toBe(expected);
   });
 
-  test.skip('another verse', () => {
+  test('another verse', () => {
     const expected =
       '3 bottles of beer on the wall, ' +
       '3 bottles of beer.\n' +
@@ -19,7 +19,7 @@ describe('Bottles', () => {
     expect(new Bottles().verse(3)).toBe(expected);
   });
 
-  test.skip('verse 2', () => {
+  test('verse 2', () => {
     const expected =
       '2 bottles of beer on the wall, ' +
       '2 bottles of beer.\n' +
@@ -28,7 +28,7 @@ describe('Bottles', () => {
     expect(new Bottles().verse(2)).toBe(expected);
   });
 
-  test.skip('verse 1', () => {
+  test('verse 1', () => {
     const expected =
       '1 bottle of beer on the wall, ' +
       '1 bottle of beer.\n' +
@@ -81,7 +81,7 @@ describe('Bottles', () => {
 
   test.skip('the whole song', () => {
     const expected =
-`99 bottles of beer on the wall, 99 bottles of beer.
+      `99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
 
 98 bottles of beer on the wall, 98 bottles of beer.
